c5ef89b7d0b8513eb82789c8fa66d26d282f8871
dawnfire05 <dawnfire05@naver.com>
2024-07-25 08:46:17 +0900

    feat: added user module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    return result;
+  }
+}
