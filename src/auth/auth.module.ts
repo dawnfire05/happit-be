a20d96fe52d5ed3793c1869659def384734965ad
dawnfire05 <dawnfire05@naver.com>
2024-07-26 15:48:41 +0900

    feat: jwt feature

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '10m' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+import process from 'process';
+
+export const jwtConstants = {
+  secret: process.env.JWT_CONSTANTS,
+};
