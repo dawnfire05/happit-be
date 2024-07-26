85c0107cf2f402250cc75bf0385bb0b3fbc40e08
dawnfire05 <dawnfire05@naver.com>
2024-07-26 11:47:18 +0900

    fix: no unused variable error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.getUserByUserName(username);
     if (user && (await bcrypt.compare(pass, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
