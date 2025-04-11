ffe62b6f7e962a50de95f38bd620c372a87c5f75
dawnfire05 <dawnfire05@naver.com>
2025-04-12 05:08:17 +0900

    feat: 애플리케이션에 로깅 인터셉터 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,19 @@ import { HabitModule } from './habit/habit.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { FcmService } from './fcm/fcm.service';
 import { RecordModule } from './record/record.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule, HabitModule, FirebaseModule, RecordModule],
   controllers: [AppController],
-  providers: [AppService, FcmService],
+  providers: [
+    AppService,
+    FcmService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
